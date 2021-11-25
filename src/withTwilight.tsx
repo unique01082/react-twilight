e9e2d17544ba97cd6add2acabb5227f443c99124
Bao Le <>
2021-11-25 13:56:12 +0700

    feat: bump version to 0.4.2

--- src/withTwilight.tsx
+++ src/withTwilight.tsx
@@ -11,8 +11,15 @@ interface ShouldForwardProp {
 
 // true => pass to component
 // false => skip
-const shouldForwardProp: ShouldForwardProp = (prop, { ignoreProps = [] }) =>
-  !(!ignoreProps.includes(prop) && parsersManager.isPropSupported(prop))
+const shouldForwardProp: ShouldForwardProp = (prop, { ignoreProps = [] }) => {
+  if (ignoreProps.includes(prop)) {
+    return true
+  }
+  if (parsersManager.isPropSupported(prop) || prop === 'ignoreProps') {
+    return false
+  }
+  return true
+}
 
 export default (Component: any) =>
   styled(Component).withConfig({
