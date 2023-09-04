76c57a2befe7acda994e9ba4e860babfe6a067d1
Bao Le <>
2023-09-04 23:14:46 +0700

    v0.6.0

--- src/core/withTwilight.tsx
+++ src/core/withTwilight.tsx
@@ -1,10 +1,10 @@
-import styled, { StyledConfig } from "styled-components";
+import styled, { ShouldForwardProp } from "styled-components";
 import { parsersManager } from "./parsersManager";
 import { twilight } from "./twilight";
 
 // true => pass to component
 // false => skip
-const shouldForwardProp: StyledConfig<any>["shouldForwardProp"] = (prop) =>
+const shouldForwardProp: ShouldForwardProp<any> = (prop: any) =>
   !(parsersManager.isPropSupported(prop) || prop === "ignoreProps");
 
 export const withTwilight = (Component: Parameters<typeof styled>[0]) =>
